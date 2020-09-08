1d1dd759852d65730293650103184d3bbde062b2
Max Lepin <max.lepin@dn-analytics.com>
2020-09-08 11:06:12 +0200

    second commit

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
 chrome.runtime.onInstalled.addListener(function() {
 console.log("Thanks for installing Fuel Calculator.")
 
-//The extension will need permission to access the declarativeContent API in its manifest.
+
+//Note : the extension will need permission to access the declarativeContent API in its manifest.
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   chrome.declarativeContent.onPageChanged.addRules([{
+  //Web pages where the user can interact with the extension
+  //The icon will be in color instead of grey on those pages.
     conditions: [new chrome.declarativeContent.PageStateMatcher({
-      //Web pages where the user can interact with the extension - The icon will be in color instead of grey.
-      pageUrl: {hostEquals: 'www.google.com'},
-      //TODO : restrict interactivty to google.com/maps
-    })
+      pageUrl: {urlContains: 'www.google.com/maps'},
+    }), 
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostContains: 'www.openstreetmap.org'},
+      }),
+
     ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
   }]);
--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
+var kms;
+ let currency = " €";
+ let litersPer100Km = 5.5 ;
+ let CostFuelPerLiter = 1.35 ;
 
+
+//Form handler
+document.getElementById('form').addEventListener("submit",function(e) {
+    e.preventDefault(); // before the code
+    /* do what you want with the form */
+    kms = document.forms["form"]["distance"].value;
+    console.log("Got value ", kms)
+    // Should be triggered on form submit
+    document.getElementById("result").innerHTML = ((litersPer100Km/100)*kms * CostFuelPerLiter) + currency  ;
+  });
