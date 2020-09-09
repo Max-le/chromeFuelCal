6118c4fe9e16af69402fe7a2b895b8961f48bb8c
Max Lepin <max.09@outloook.com>
2020-09-09 11:49:46 +0200

    Options page, options saving in storage

---
+++ options.js
@@ -0,0 +1,39 @@
+var userCurrencyInput = document.getElementById("userCurrency");
+var userLiterPerKmInput= document.getElementById("userLitersPerKm");
+var userCostLiterPerFuelInput = document.getElementById("userCostLiterOfFuel");
+
+
+function get_user_options(){
+    console.log("Options function")
+    chrome.storage.sync.get("currency", function(data){
+        userCurrencyInput.value = data.currency;
+    console.log(data.currency);
+      })
+      chrome.storage.sync.get("litersPerKm", function(data){
+        userLiterPerKmInput.value = data.litersPerKm;
+        console.log(data.litersPerKm);
+
+      })
+      chrome.storage.sync.get("costLiterOfFuel", function(data){
+        userCostLiterPerFuelInput.value = data.costLiterOfFuel;
+        console.log(data.costLiterOfFuel);
+
+      })
+}
+
+//Listener for Save Button
+document.getElementById("saveButton").addEventListener("click", set_user_options)
+
+
+function set_user_options(){ 
+    chrome.storage.sync.set({
+        currency: userCurrencyInput.value,
+        litersPerKm: userLiterPerKmInput.value, 
+        costLiterOfFuel:userCostLiterPerFuelInput.value} , function() {
+            alert("Values updated.");
+      });
+}
+
+
+//Get executed when Options page opened/refreshed
+get_user_options()
